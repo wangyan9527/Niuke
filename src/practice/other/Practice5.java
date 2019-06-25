7a688016a967ca4b46f9562d6f4f5ae3c688d1ef
wangyan <wangyan02.huatu.com>
2019-06-25 19:59:36 +0800

    求一个数的指定次方

--- src/practice/other/Practice5.java
+++ src/practice/other/Practice5.java
@@ -6,11 +6,30 @@ package practice.other;
 public class Practice5 {
 
     public static void main(String[] args) {
-
+        System.out.println(Power(2, -3));
     }
 
+    /**
+     * 求次方可以通过指数来求
+     * 例:10的14次方,14的二进制为1110,10的14次方可以分解为10的1000次方*10的100次方*10的10次方
+     *
+     * @param base
+     * @param exponent
+     * @return
+     */
     public static double Power(double base, int exponent) {
-        return 0;
+        if (base == 0) {
+            return 0;
+        } else {
+            double result = 1;
+            int positive = Math.abs(exponent);
+            for (int i = 1; i <= positive; i <<= 1) {
+                if ((i & positive) == i) {
+                    result *= Math.pow(base, i);
+                }
+            }
+            return exponent > 0 ? result : 1 / result;
+        }
     }
 
 }
